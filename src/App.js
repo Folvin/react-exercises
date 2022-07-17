3bd45a1f15d332217e7c11b40ff2db5e1aa4da15
Kevin Alessi <minicarota15@gmail.com>
2022-07-17 10:46:57 +0200

    feat: esercizio props-04 completato

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export class App extends React.Component {
         return (
             <div>
                 <Hello />
-                <Welcome age={19}/>
+                <Welcome name={<strong>Chiara</strong>} age={20}/>
             </div>
         );
     }
