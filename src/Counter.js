67be4642ad1928daf5ba3ab79efbf5f91097d916
Kevin Alessi <minicarota15@gmail.com>
2022-07-18 17:55:07 +0200

    feat: esercizio state-04 completato

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {CounterDisplay} from "./CounterDisplay";
 
 export class Counter extends React.Component {
     state = {
@@ -15,6 +16,6 @@ export class Counter extends React.Component {
     }
 
     render() {
-        return <h1>counter: {this.state.count}</h1>;
+        return <CounterDisplay count={this.state.count} />;
     }
 }
