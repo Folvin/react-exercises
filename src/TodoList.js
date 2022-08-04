7c44efeb0d92e8ce6d61bcf0cd24308a2fec0482
Kevin Alessi <minicarota15@gmail.com>
2022-08-04 09:54:39 +0200

    lists-06

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,12 +8,21 @@ export class TodoList extends React.Component {
     inputRef = createRef();
 
     itemManager = (e) => {
-        if (e.target.name === "add") {
-            let inputValue = this.inputRef.current.value;
-            const newItemsState = [...this.state.items, inputValue];
-            this.setState({items: [...newItemsState]});
-        } 
-        this.inputRef.current.value = "";
+        if (e.target.name === "remove") {
+            const allLiEl = e.target.closest("ul").children;
+            const liEl = e.target.closest("li");
+            const liElIndex = Array.from(allLiEl).indexOf(liEl);
+            const newItemsState = [...this.state.items];
+            newItemsState.splice(liElIndex, 1);
+            this.setState({items: newItemsState});
+        } else {
+            if (e.target.name === "add") {
+                let inputValue = this.inputRef.current.value;
+                const newItemsState = [...this.state.items, inputValue];
+                this.setState({items: newItemsState});
+            }
+            this.inputRef.current.value = "";
+        }
     };
 
     render() {
@@ -21,12 +30,25 @@ export class TodoList extends React.Component {
             <div>
                 <ul>
                     {this.state.items.map((item, index) => {
-                        return <li key={index}>{item}</li>;
+                        return (
+                            <li key={index}>
+                                {item}
+                                <button
+                                    name="remove"
+                                    onClick={this.itemManager}>
+                                    remove
+                                </button>
+                            </li>
+                        );
                     })}
                 </ul>
                 <input ref={this.inputRef} type="text" />
-                <button name="add" onClick={this.itemManager} type="button">add item</button>
-                <button name="reset" onClick={this.itemManager} type="button">reset</button>
+                <button name="add" onClick={this.itemManager} type="button">
+                    add item
+                </button>
+                <button name="reset" onClick={this.itemManager} type="button">
+                    reset
+                </button>
             </div>
         );
     }
