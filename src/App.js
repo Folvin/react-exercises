f2aa17af3a3e48a2b9bd2b2dd2f0600754da078f
Kevin Alessi <minicarota15@gmail.com>
2022-08-12 23:29:19 +0200

    function-component-01

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React from "react";
-import DisplayLanguage from "./DisplayLanguage";
-import {LanguageContext} from "./LanguageContext";
+import {Welcome} from "./Welcome";
 
-export class App extends React.Component {
-    state = {
-        language: "english"
-    };
-
-    languageChangeHandler = (e) => {
-        this.setState({language: e.target.value});
-    };
-
-    render() {
-        return (
-            <div>
-                <select name="Language" onChange={this.languageChangeHandler}>
-                    <option value="english">{this.state.language === "italiano" ? "inglese" : "english"}</option>
-                    <option value="italiano">{this.state.language === "italiano" ? "italiano" : "italian"}</option>
-                </select>
-                <LanguageContext.Provider value={this.state.language}>
-                    <DisplayLanguage />
-                </LanguageContext.Provider>
-            </div>
-        );
-    }
+export function App(props) {
+    return (
+        <div>
+            <Welcome name="chiara"/>
+        </div>
+    );
 }
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+export function Welcome({name}) {
+    return <div>
+        <h1>welcome, {name}</h1>
+    </div>;
+}
