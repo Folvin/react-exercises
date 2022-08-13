9e2284b3001286674d069d7ccd3c70ea27b16b32
Kevin Alessi <minicarota15@gmail.com>
2022-08-13 14:43:46 +0200

    useeffect-03

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import {useState} from "react";
-import {Counter} from "./Counter";
+import {GithubUser} from "./GithubUser";
 
 export function App() {
-    const [counterToggle, setCounterToggle] = useState(true);
-    function toggleHandler() {
-        setCounterToggle(toggle => !toggle)
-    }
     return (
         <div>
-            <button onClick={toggleHandler}>toggle counter</button>
-            {counterToggle && <Counter />}
+            <GithubUser username="folvin"/>
         </div>
     );
 }
