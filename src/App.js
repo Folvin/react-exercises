30c953cce0b6fb9a8d67994be87b53b627cdcc6f
Kevin Alessi <minicarota15@gmail.com>
2022-08-14 03:37:16 +0200

    usecontext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import {CarDetails} from "./CarDetails";
+import { useState } from "react";
+import {DisplayLanguage} from "./DisplayLanguage";
+import {LanguageContext} from "./LanguageContext";
 
 export function App() {
+
+    const [language, setLanguage] = useState("english")
+
+    function languageChangeHandler(e) {
+        setLanguage(language => e.target.value)
+    }
+
     return (
         <div>
-            <CarDetails
-                initialData={{
-                    model: "Audi A1",
-                    year: 2022,
-                    color: "metallic gray",
-                }}
-            />
+            <select name="language" onChange={languageChangeHandler}>
+                <option value="english">{language === "italiano" ? "inglese" : "english"}</option>
+                <option value="italiano">{language === "italiano" ? "italiano" : "italian"}</option>
+            </select>
+            <LanguageContext.Provider value={language}>
+                <DisplayLanguage />
+            </LanguageContext.Provider>
         </div>
     );
 }
