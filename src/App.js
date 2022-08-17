1012d7621015230a6531b17fa5d10a00f164eba5
Kevin Alessi <minicarota15@gmail.com>
2022-08-17 10:58:00 +0200

    react-router-05

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ export function App() {
                 <Route path="/" element={<Welcome name="Usuku" />} />
                 <Route path="/counter" element={<ClickCounter />} />
                 <Route path="users/:username" element={<GithubUser />} />
+                <Route path="*" element={<h1>route not found</h1>} />
             </Routes>
             <ul>
                 <li><Link to="/">go back to /</Link></li>
                 <li><Link to="counter">render ClickCounter</Link></li>
                 <li><Link to="users/folvin">render GithubUser</Link></li>
+                <li><Link to="nowhere">go nowhere</Link></li>
             </ul>
         </div>
     );
