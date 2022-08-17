15c176368484889d5130b9c149c11fcccd5a852e
Kevin Alessi <minicarota15@gmail.com>
2022-08-17 10:20:17 +0200

    react-router-01

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import {FilteredList} from "./FilteredList";
+import {Route, Routes} from "react-router-dom";
+import {Welcome} from "./Welcome";
 
 export function App() {
     return (
         <div>
-            <FilteredList
-                usersList={[
-                    {name: "HalfBlood NIK", id: "#4364", age: 20},
-                    {name: "Usuku", id: "#5541", age: 20},
-                    {name: "ApocEnd", id: "#7758", age: 21},
-                    {name: "Folvin the folf", id: "#9892", age: 19},
-                    {name: "tizio a caso", id: "#bho", age: 14},
-                ]}
-            />
+            <Routes>
+                <Route path="/" element={<Welcome name="Usuku" />} />
+            </Routes>
         </div>
     );
 }
