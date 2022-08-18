1fe98e870042d5d47fc533aded4c49aa6e12a660
Kevin Alessi <minicarota15@gmail.com>
2022-08-18 19:10:15 +0200

    swr-03

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,10 +1,11 @@
 import {useGithubUser} from "./useGithubUser";
 
 export function GithubUser({username}) {
-    const {data} = useGithubUser(username);
+    const {data, refreshData} = useGithubUser(username);
     return (
         <div>
             {data && <h1>{data.id}</h1>}
+            <button onClick={refreshData}>refresh</button>
         </div>
     );
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,9 +1,14 @@
 import useSWR from "swr";
 
 export function useGithubUser(username) {
-    const fetcher = (url) => username && fetch(url).then((response) => response.json());
-    const {data} = useSWR(`https://api.github.com/users/${username}`, fetcher);
+    const fetcher = (url) =>
+        username && fetch(url).then((response) => response.json());
+    const {data, mutate} = useSWR(
+        `https://api.github.com/users/${username}`,
+        fetcher
+    );
     return {
         data: data,
+        refreshData: mutate,
     };
 }
