1e0edea5cb9f5fae5876ac1184e6de71f5a0eb07
Kevin Alessi <minicarota15@gmail.com>
2022-08-18 19:07:47 +0200

    swr-02

--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,8 +1,7 @@
 import useSWR from "swr";
 
 export function useGithubUser(username) {
-    const fetcher = (url) => fetch(url).then((response) => response.json());
-
+    const fetcher = (url) => username && fetch(url).then((response) => response.json());
     const {data} = useSWR(`https://api.github.com/users/${username}`, fetcher);
     return {
         data: data,
